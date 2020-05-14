dc6397bb41d760c51373ada2eae369bb7c831baa
Evelyn Fu <evelynfu15@gmail.com>
2020-05-14 01:27:32 -0400

    i think it's working?

--- Assets/Scripts/CreateRoom/CreateRoom.cs
+++ Assets/Scripts/CreateRoom/CreateRoom.cs
@@ -19,7 +19,6 @@ public class CreateRoom : MonoBehaviour
 
         if (PhotonNetwork.CreateRoom(RoomName.text, roomOptions, TypedLobby.Default)){
             print("create room successsfully sent.");
-            PhotonNetwork.JoinLobby(TypedLobby.Default);
         }
         else
         {
@@ -36,7 +35,6 @@ public class CreateRoom : MonoBehaviour
     private void OnCreatedRoom()
     {
         print("Room created successfully");
-        PhotonNetwork.JoinLobby(TypedLobby.Default);
         print("inside lobby " + PhotonNetwork.insideLobby);
     }
 
